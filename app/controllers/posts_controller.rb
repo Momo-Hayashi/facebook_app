3ed02cba698d7de7b549920f2608c2a45aa731f5
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-06-10 19:54:35 +0900

    投稿、編集、削除、一覧表示、詳細画面の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,48 @@
 class PostsController < ApplicationController
+  before_action :set_posts, only:[:show, :edit, :destroy, :update]
   def index
     @posts = Post.all
   end
   def new
-    @post = Post.new
+    if params[:back]
+      @post = Post.new(post_params)
+    else
+      @post = Post.new
+    end
   end
   def confirm
-
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
   end
   def create
     @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: '投稿しました！'
+      else
+        render :new
+      end
+    end
   end
   def destroy
-
+    @post.destroy
+    redirect_to posts_path, notice: '削除しました！'
   end
-  def show
-
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice:"アップデートしました！"
+    else
+      render :edit
+    end
   end
+
   private
   def post_params
-
+    params.require(:post).permit(:content, :image, :image_cache)
+  end
+  def set_posts
+    @post = Post.find(params[:id])
   end
 end
